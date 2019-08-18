766c25dfd4580455ff82101b0944c5d9186d6706
sajrus33 <brianwala22@gmail.com>
2019-08-18 18:35:49 +0200

    Updates

---
+++ precache-manifest.49e2748400756294ec3f6b7aefe93625.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f64b149c6c9f75148299a3c2e614cd5",
+    "url": "/React-map/index.html"
+  },
+  {
+    "revision": "d472b5751cfcc1960357",
+    "url": "/React-map/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "df6f5c6fe97e61eceae5",
+    "url": "/React-map/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d472b5751cfcc1960357",
+    "url": "/React-map/static/js/2.4f7e971c.chunk.js"
+  },
+  {
+    "revision": "df6f5c6fe97e61eceae5",
+    "url": "/React-map/static/js/main.b53e1b02.chunk.js"
+  },
+  {
+    "revision": "64dbabfad8d1a4137a57",
+    "url": "/React-map/static/js/runtime~main.12d96945.js"
+  }
+]);
